ae83eedafb4f53d441d0834cb5424b8601584a79
annn123 <132078483+annn123@users.noreply.github.com>
2024-04-19 00:00:16 +0800

    計算機

--- first_class.js
+++ first_class.js
@@ -1,2 +1,22 @@
-var username = prompt("請輸入名字")
-document.write("welcome to zlcsc "+ username)
+function appendToOutput(value) {
+    document.getElementById("output").value += value;
+  }
+  
+  function clearOutput() {
+    document.getElementById("output").value = "";
+  }
+  
+  function calculate() {
+    let expression = document.getElementById("output").value;
+    let result;
+  
+    try {
+      result = eval(expression);
+      if (isNaN(result) || !isFinite(result)) {
+        throw new Error("Invalid expression");
+      }
+      document.getElementById("output").value = result;
+    } catch (error) {
+      document.getElementById("output").value = "Error";
+    }
+  }
